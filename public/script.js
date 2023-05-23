039cfd202f50a092adf7f923984c114bc0e2520e
Vishal Agarwal <agarwalvishal1994@gmail.com>
2023-05-23 23:32:37 +0530

    Setup video calling

--- public/script.js
+++ public/script.js
@@ -1,17 +1,52 @@
+const socket = io('/'); // Socket will connect to '/' - the root path of our application.
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
-myVideo.muted = false;
+myVideo.muted = true;
 
-let myVideoStream;
+const peer = new Peer(undefined, {
+  host: '/',
+  port: '3000',
+  path: '/peerjs'
+});
+
+const peers = {};
 
 // ask for audio and video webrtc permissions
 navigator.mediaDevices.getUserMedia({
-  audio: true,
+  audio: false, // TODO: Revert to true
   video: true
 }).then(stream => {
-  myVideoStream = stream;
   addVideoStream(myVideo, stream);
+  
+  peer.on('call', call => {
+    const video = document.createElement('video');
+
+    call.answer(stream);
+
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    });
+  });
+  
+  socket.on('user-connected', (userId) => {
+    connectToNewUser(userId, stream);
+  });
+}).catch(error => {
+  console.log('Failed to get local stream', error);
 });
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) {
+    peers[userId].close();
+  }
+});
+
+peer.on('open', id => {
+  console.log('peer id: ' + id);
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+// Helper functions
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
@@ -19,4 +54,22 @@ const addVideoStream = (video, stream) => {
     video.play();
   })
   videoGrid.append(video);
+}
+
+const connectToNewUser = (userId, stream) => {
+  call = peer.call(userId, stream);
+  const video = document.createElement('video');
+
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+  
+  call.on('close', () => {
+    video.remove();
+    console.log('User disconnected');
+  });
+
+  // Keep track of connected users and which call they're connected to
+  // so that they can be removed from the video grid when they disconnect
+  peers[userId] = call;
 }
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import http from 'http';
+import { Server } from 'socket.io';
+import { ExpressPeerServer } from 'peer';
 
 const app = express();
 const server = http.Server(app);
+const io = new Server(server);//, {cors: {origin: '*'}});
+
+// Combining peer with existing express app
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 
 app.set('view engine', 'ejs');
 
 // Use the pulbic directory to serve static files
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -20,4 +29,16 @@ app.get('/:room', (req, res) => {
 
 server.listen(3000, () => {
   console.log('app is running on localhost:3000');
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(`joined room: ${roomId} with user: ${userId}`);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+    
+    socket.on('disconnect', () => {
+      socket.broadcast.to(roomId).emit('user-disconnected', userId);
+    });
+  });
 });
